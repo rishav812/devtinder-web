84c51833816ee4807f34d72b76bc3a0a1f76cf05
rishav812 <kumarishav812@gmail.com>
2025-07-23 18:48:12 +0530

    feat: implement styled login component with card layout

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="flex justify-center my-20">
+      <div className="card w-96 bg-base-200 card-xs shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Sigin</h2>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login
