39f06e8825c29118a226b894199a00779b846626
Rishav Kumar <kumarishav812@gmail.com>
2025-07-24 10:51:04 +0530

    fix: correct spelling of "Signin" in Login component and improve input fields style: update Navbar background color for better contrast

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
     <div className="flex justify-center my-20">
       <div className="card w-96 bg-base-200 card-xs shadow-sm">
         <div className="card-body">
-          <h2 className="card-title">Sigin</h2>
+          <h2 className="card-title">Signin</h2>
           <div className="justify-end card-actions">
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="input input-bordered w-full mb-3 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary transition duration-200"
+            />
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="input input-bordered w-full mb-3 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary transition duration-200"
+            />
             <button className="btn btn-primary">Login</button>
           </div>
         </div>
@@ -15,4 +25,4 @@ function Login() {
   );
 }
 
-export default Login
+export default Login;
